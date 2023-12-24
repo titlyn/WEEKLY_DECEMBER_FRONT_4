18c452640dca536c7a8acd6f3cb1f39878ff6918
Tolotra Mandresy <tolotramandresy39@gmail.com>
2023-12-24 20:26:52 +0300

    loader and aos done

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,10 @@ import { setUpLoader } from './modules/loader.js'
 import { setUpTheme } from './modules/theme.js'
 import {setUpNav} from './modules/nav.js'
 import { setUpScrollSpy } from './modules/scroll-spy.js'
+import { setUpAOS } from './modules/aos.js'
 
 setUpTheme()
 setUpLoader()
 setUpNav()
 setUpScrollSpy()
+setUpAOS()
