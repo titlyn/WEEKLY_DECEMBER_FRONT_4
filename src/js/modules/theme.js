03000069d5a93c53fdfb53c97d695fd0245de660
Tolotra Mandresy <tolotramandresy39@gmail.com>
2023-12-24 12:43:47 +0300

    theme toggler done

---
+++ src/js/modules/theme.js
@@ -0,0 +1,27 @@
+const storageKey = 'figma-land-isLight'
+const rootEl = document.querySelector('html')
+const lightToggler = document.querySelector('#light')
+const darkToggler = document.querySelector('#dark')
+
+function setToLightTheme(isLight = false){
+    console.log(isLight);
+    if(isLight) {
+        rootEl.classList.add('light')
+    }
+    else {
+        rootEl.classList.remove('light')
+    }
+    saveTheme(isLight)
+}
+
+function saveTheme(isLight = false){
+    localStorage.setItem(storageKey, isLight)
+}
+
+export function setUpTheme(){
+    const save = localStorage.getItem(storageKey) || false
+    setToLightTheme(save)
+
+    lightToggler.addEventListener('click', () => setToLightTheme(true))
+    darkToggler.addEventListener('click', () => setToLightTheme(false))
+}
