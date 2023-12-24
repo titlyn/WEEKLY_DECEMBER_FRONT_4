96fdc90c151f0bdd486250427b9c5d1da9d858bd
Tolotra Mandresy <tolotramandresy39@gmail.com>
2023-12-24 08:31:41 +0300

    fix header and work

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,1 @@
-const navButton = document.getElementById("menu");
-const links = document.querySelector(".links");
-
-
-
-navButton.addEventListener("click", function() {
-    links.classList.toggle("active");
-})
-
-const menuLinks = document.querySelectorAll(".links a");
-
-menuLinks.forEach(link => {
-  link.addEventListener("click", function() {
-    links.classList.remove("active");
-  });
-});
+import * as nav from './modules/nav.js'
